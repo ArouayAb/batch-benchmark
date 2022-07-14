2b0397804f97bdaffb4719c1ef2450527abfc37e
unknown <arouayab@gmail.com>
2022-07-14 19:02:38 +0100

    Text python script added

--- jobs/scripts/transaction.py
+++ jobs/scripts/transaction.py
@@ -1,1 +1,25 @@
-print("Py Script executed")
+import mysql.connector
+import json
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="password",
+  database="batchbenchmark"
+)
+
+transactions_file = open('jobs/batches/transactions.json')
+
+transactions = json.load(transactions_file)
+
+print(transactions)
+
+mycursor = mydb.cursor()
+for transaction in transactions:
+    sql = f"UPDATE clients SET balance = balance {'+' if transaction['OpType']=='IN' else '-'} {transaction['Amount']} WHERE code = {transaction['Code']}"
+
+    mycursor.execute(sql)
+
+    mydb.commit()
+
+print(mycursor.rowcount, "record(s) affected")
